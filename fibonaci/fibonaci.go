56bcee02ab639abf624db7ce067dc602e2449987
Nisarg2061 <khodkenisarg@gmail.com>
2024-07-13 01:27:13 +0530

    Fibonaci through slice

--- fibonaci/fibonaci.go
+++ fibonaci/fibonaci.go
@@ -1,5 +1,9 @@
 package main
 
-func main(){
-  
+func fib1(p int) int{
+  seq := []int {0,1}
+  for len(seq) <= p {
+    seq = append(seq, seq[len(seq)-1] + seq[len(seq)-2])
+  }
+  return seq[len(seq)-1]
 }
--- fibonaci/fibonaci_test.go
+++ fibonaci/fibonaci_test.go
@@ -2,6 +2,10 @@ package main
 
 import "testing"
 
-func Test__(t *testing.T){
-    
+func TestFeb1(t *testing.T){
+  result := fib1(6)
+
+  if result != 8{
+    t.Error("Expected value 8 got", result)
+  }
 }
